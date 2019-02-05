ba0b8e30bf616519ab24f68e31a01aa09e289983
GLENN HORAN <GLENN.HORAN@LIBERTY-IT.CO.UK>
2019-02-05 16:46:14 +0000

    updated chatbot numbers

--- chatbot.py
+++ chatbot.py
@@ -2,13 +2,12 @@ from numberGame import numberGame
 response = ""
 name = raw_input("Hello, what is your name? \n")
 print("Hello " + name + "I am a VERY basic chat bot. Just like Amazon's Alexa, except I am not that smart =(")
+
 while response != "9":
     response = raw_input("""What would you like to do?
   1. What's the weather today?
   2. let's play a number guessing game 
-  3. let's play hangman
-  4. quit\n""")
-
+  9. quit\n""")
 
     if response == "1":
         print("\n Well it's Lisburn, so it's probably raining\n")
